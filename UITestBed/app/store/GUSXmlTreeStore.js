76d3d0bd309af355484a6c504f9f08954085c033
feng hua <frankfenghua@gmail.com>
2012-05-06 19:15:01 -0400

    [CHG] print treestore on('append') event, see http://jsfiddle.net/QvaMG/195/

--- UITestBed/app/store/GUSXmlTreeStore.js
+++ UITestBed/app/store/GUSXmlTreeStore.js
@@ -50,6 +50,10 @@ Ext.define('MyApp.store.GUSXmlTreeStore', {
                 expand:{
                     fn: me.onXmltreestoreExpand,
                     scope: me
+                },
+                append:{
+                    fn: me.onXmltreestoreAppend,
+                    scope:me
                 }
             }
         }, cfg)]);
@@ -60,5 +64,34 @@ Ext.define('MyApp.store.GUSXmlTreeStore', {
     },
     onXmltreestoreExpand: function(treestore, options) {
         console.log("onXmltreestoreExpand()" + " :  treestore.data.label = " + treestore.data.label);
+    },
+    // Each demo.UserModel instance will be automatically
+    // decorated with methods/properties of Ext.data.NodeInterface
+    // (i.e., a "node"). Whenever a UserModel node is appended
+    // to the tree, this TreeStore will fire an "append" event.
+    // http://jsfiddle.net/QvaMG/195/
+    onXmltreestoreAppend: function( thisNode, newChildNode, index, eOpts ) {
+//        console.log("onXmltreestoreAppend()" + " :  treestore.data.label = " + treestore.data.label);
+        // If the node that's being appended isn't a root node, then we can
+        // assume it's one of our UserModel instances that's been "dressed
+        // up" as a node
+        if( !newChildNode.isRoot() ) {
+
+            // The node is a UserModel instance with NodeInterface
+            // properties and methods added. We want to customize those
+            // node properties  to control how it appears in the TreePanel.
+
+            // A user "item" shouldn't be expandable in the tree
+            newChildNode.set('leaf', true);
+
+            // Use the model's "name" value as the text for each tree item
+            newChildNode.set('text', newChildNode.get('label'));
+
+            // Use the model's profile url as the icon for each tree item
+//            newChildNode.set('icon', newChildNode.get('profile_image_url'));
+//            newChildNode.set('cls', 'demo-userNode');
+//            newChildNode.set('iconCls', 'demo-userNodeIcon');
+        }
+
     }
 });
