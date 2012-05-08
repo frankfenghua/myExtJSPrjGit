f12f32488aa66badfa3c26baad47215dd2082d44
feng hua <frankfenghua@gmail.com>
2012-05-07 23:03:20 -0400

    [CHG] http://javaclaus.wordpress.com/2010/10/12/extjs-xml-and-the-dom/

--- UITestBed/app/store/GUSXmlTreeStore.js
+++ UITestBed/app/store/GUSXmlTreeStore.js
@@ -39,6 +39,8 @@ Ext.define('MyApp.store.GUSXmlTreeStore', {
                 reader: {
                     type: 'xml',
                     root: 'explorer',
+                    //http://javaclaus.wordpress.com/2010/10/12/extjs-xml-and-the-dom/
+//                    record: 'node[@type="GUIDELINE"]'
                     record: 'node'
                 }
             },
